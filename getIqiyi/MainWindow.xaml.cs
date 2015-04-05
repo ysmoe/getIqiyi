3e15d45512617327f58f8751e99e34de1ad1ef42
imi415 <imi415.public@gmail.com>
2015-04-06 02:29:24 +0900

    新增功能
    加入了PPTV的Flash提取功能

--- getIqiyi/MainWindow.xaml.cs
+++ getIqiyi/MainWindow.xaml.cs
@@ -40,41 +40,22 @@ namespace getIqiyi
 
         private void getClick(object sender, RoutedEventArgs e)
         {
+            playUrlString = playUrl.Text;
             if (String.IsNullOrEmpty(playUrl.Text))
             {
-                MessageBox.Show("不要恶意卖萌的说~", "请检查URL", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                MessageBox.Show("请填写URL", "不要恶意卖萌的说~", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+            }
+            else if(chooseIqiyi.IsChecked == true)
+            {
+                sendGetIqiyi();
+            }
+            else if(choosePptv.IsChecked == true)
+            {
+                sendGetPptv();
             }
             else
             {
-                try
-                {
-                    playUrlString = playUrl.Text;
-                    playUrlByte = Encoding.UTF8.GetBytes(playUrlString);
-                    HttpWebRequest getRequest = (HttpWebRequest)WebRequest.Create(new Uri(playUrlString));
-                    getRequest.CookieContainer = new CookieContainer();
-                    getRequest.Method = "GET";
-                    getRequest.Accept = "text/html, application/xhtml+xml, */*";
-                    getRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko";
-                    HttpWebResponse getResponse = (HttpWebResponse)getRequest.GetResponse();
-                    getResponseStreamReader = new StreamReader(getResponse.GetResponseStream(), Encoding.UTF8);
-                    recvString = getResponseStreamReader.ReadToEnd();
-                    getResponseStreamReader.Close();
-                    getResponse.Close();
-                    Match videoId = Regex.Match(recvString, @"data-player-videoid=.(.*)..");
-                    string vid = videoId.Groups[1].Value;
-                    Match videoTvid = Regex.Match(recvString, @"data-player-tvid=.(.*)..");
-                    string tvid = videoTvid.Groups[1].Value;
-                    Match albumId = Regex.Match(recvString, @"data-player-albumid=.(.*)..");
-                    string aid = albumId.Groups[1].Value;
-                    Match swf = Regex.Match(playUrlString, @"http://www.iqiyi.com/(.*).....#");
-                    string swfName = swf.Groups[1].Value;
-                    flashUrlString = "http://player.video.qiyi.com/" + vid + "/0/0/" + swfName + ".swf-ismember=false-albumId=" + aid + "-tvId=" + tvid + "-isPurchase=0-cnId=4";
-                    flashUrl.Text = flashUrlString;
-                }
-                catch (Exception netException)
-                {
-                    MessageBox.Show(netException.Message);
-                }
+                MessageBox.Show("请选择视频源", "不要恶意卖萌的说~", MessageBoxButton.OK, MessageBoxImage.Asterisk);
             }
         }
 
@@ -87,6 +68,67 @@ namespace getIqiyi
         private void clearButtonClick(object sender, RoutedEventArgs e)
         {
             flashUrl.Text = "";
+        }
+        public void sendGetIqiyi()
+        {
+            try
+            {
+                playUrlByte = Encoding.UTF8.GetBytes(playUrlString);
+                HttpWebRequest getRequest = (HttpWebRequest)WebRequest.Create(new Uri(playUrlString));
+                getRequest.CookieContainer = new CookieContainer();
+                getRequest.Method = "GET";
+                getRequest.Accept = "text/html, application/xhtml+xml, */*";
+                getRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko";
+                HttpWebResponse getResponse = (HttpWebResponse)getRequest.GetResponse();
+                getResponseStreamReader = new StreamReader(getResponse.GetResponseStream(), Encoding.UTF8);
+                recvString = getResponseStreamReader.ReadToEnd();
+                getResponseStreamReader.Close();
+                getResponse.Close();
+                Match videoId = Regex.Match(recvString, @"data-player-videoid=.(.*)..");
+                string vid = videoId.Groups[1].Value;
+                Match videoTvid = Regex.Match(recvString, @"data-player-tvid=.(.*)..");
+                string tvid = videoTvid.Groups[1].Value;
+                Match albumId = Regex.Match(recvString, @"data-player-albumid=.(.*)..");
+                string aid = albumId.Groups[1].Value;
+                Match swf = Regex.Match(playUrlString, @"http://www.iqiyi.com/(.*).....#");
+                string swfName = swf.Groups[1].Value;
+                flashUrlString = "http://player.video.qiyi.com/" + vid + "/0/0/" + swfName + ".swf-ismember=false-albumId=" + aid + "-tvId=" + tvid + "-isPurchase=0-cnId=4";
+                flashUrl.Text = flashUrlString;
+            }
+            catch (Exception netException)
+            {
+                MessageBox.Show(netException.Message);
+            }
+        }
+
+        public void sendGetPptv()
+        {
+            try
+            {
+                playUrlByte = Encoding.UTF8.GetBytes(playUrlString);
+                HttpWebRequest getRequest = (HttpWebRequest)WebRequest.Create(new Uri(playUrlString));
+                getRequest.CookieContainer = new CookieContainer();
+                getRequest.Method = "GET";
+                getRequest.Accept = "text/html, application/xhtml+xml, */*";
+                getRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko";
+                HttpWebResponse getResponse = (HttpWebResponse)getRequest.GetResponse();
+                getResponseStreamReader = new StreamReader(getResponse.GetResponseStream(), Encoding.UTF8);
+                recvString = getResponseStreamReader.ReadToEnd();
+                getResponseStreamReader.Close();
+                getResponse.Close();
+                Match videoId = Regex.Match(recvString, @"\""swf"":"".............................(.*)....""}]");
+                string vid = videoId.Groups[1].Value;
+                flashUrlString = "http://player.pptv.com/v/" + vid + ".swf";
+                flashUrl.Text = flashUrlString;
+            }
+            catch (Exception netException)
+            {
+                MessageBox.Show(netException.Message);
+            }
+        }
+        private void aboutButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("作者 : imi415@U2 \nTwitter : @imi415_ \n软件更新 : https://github.com/imi415/getIqiyi \n软件主页 : https://blog.imi.moe/?p=251 ", "关于", MessageBoxButton.OK, MessageBoxImage.Asterisk);
         }
     }
 }
